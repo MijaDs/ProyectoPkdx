8e847d69cd607fd3bc9d3f73222ae6d7d76ecca4
Isaac <morabisaac@gmail.com>
2024-12-09 22:06:19 -0600

    creacion vistas templates & index
    si les da error creo que cambie las appsettings.json el acceso a la BD

--- Api_Pdx_Db_V2/Api_Pdx_Db_V2/obj/Debug/net8.0/Api_Pdx_Db_V2.AssemblyInfo.cs
+++ Api_Pdx_Db_V2/Api_Pdx_Db_V2/obj/Debug/net8.0/Api_Pdx_Db_V2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Pdx_Db_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4ecaad7088775dd6541d0040aa78a433c08bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038ed9567348bc755c185b3bdc4fd98511b4ef07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Pdx_Db_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Pdx_Db_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Api_Pdx_Db_V2/Api_Pdx_Db_V2/obj/Release/net8.0/Api_Pdx_Db_V2.AssemblyInfo.cs
+++ Api_Pdx_Db_V2/Api_Pdx_Db_V2/obj/Release/net8.0/Api_Pdx_Db_V2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_Pdx_Db_V2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0da48283cef32399f66b0b767f6d73bc2473362")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+038ed9567348bc755c185b3bdc4fd98511b4ef07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_Pdx_Db_V2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_Pdx_Db_V2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
