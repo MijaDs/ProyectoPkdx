826d0ff3998f8e1f4b2f80a045093d7a585f04cf
ruzjim <162730248+ruzjim@users.noreply.github.com>
2024-12-12 22:39:05 -0600

    3.0

--- PoyectoPokedexApi/PoyectoPokedexApi/obj/Debug/net8.0/PoyectoPokedexApi.AssemblyInfo.cs
+++ PoyectoPokedexApi/PoyectoPokedexApi/obj/Debug/net8.0/PoyectoPokedexApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PoyectoPokedexApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf39692c60cd7bbb5de1bab2c378e4a4cfa7d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee79dea4aa53483e412bc7cb3ae71e427c5fb324")]
 [assembly: System.Reflection.AssemblyProductAttribute("PoyectoPokedexApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PoyectoPokedexApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
