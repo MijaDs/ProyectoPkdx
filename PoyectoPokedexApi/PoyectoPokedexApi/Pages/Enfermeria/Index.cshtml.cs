ee79dea4aa53483e412bc7cb3ae71e427c5fb324
ruzjim <162730248+ruzjim@users.noreply.github.com>
2024-12-12 21:43:26 -0600

    creando enfermeria

---
+++ PoyectoPokedexApi/PoyectoPokedexApi/Pages/Enfermeria/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PoyectoPokedexApi.Pages.Enfermeria
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
